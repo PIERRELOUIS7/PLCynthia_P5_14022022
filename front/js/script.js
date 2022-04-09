b6b4d3d5d3b7d259b31ce362316be8b036743798
PierrelouisCynthia <pierrelouis.cynthia7@gmail.com>
2022-04-09 09:09:53 +0200

    avancement js

--- front/js/script.js
+++ front/js/script.js
@@ -18,5 +18,42 @@ const callProduct = function () {
 
 /*La fonction kanap stocke tous les produits dans un tableau*/
 const kanap = (array) => {
+    const entrerpoint = document.querySelector("#items")
+    const premierkanap = array[0]
+    const template = `<a href="./product.html?id=42">
+            <article>
+              <img src="${premierkanap.imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
+              <h3 class="productName">${premierkanap.name}</h3>
+              <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
+            </article>
+          </a>`
     //Le tableau
-    array.map(product => {
+    console.log(premierkanap)
+    entrerpoint.innerHTML = template
+}
+
+    callProduct()
+    
+        const a = document.createElement('a');
+        a.href = "somelink url"
+        /*Création de l'objet article*/
+        const article = document.createElement('article');
+        //les articles sont les enfants des liens a
+        a.appendChild(article);
+
+        /*Création de l'objet img*/
+        const imge = document.createElement("img");
+        //les images sont les enfants des articles
+        article.appendChild(imge);
+        //Récupération des données des images : image et texte
+        imge.src = product.imageUrl;
+        imge.alt = product.altTxt;
+
+        /*Création de l'objet h3*/
+        const h3 = document.createElement("h3");
+        //la class productName est ajoutée aux titres h3 
+        h3.classList.add("productName");
+        //Les h3 sont les enfants des articles
+        article.appendChild(h3);
+        //Récupération du texte des h3
+        h3.textContent = product.name;
